fcad24f7f29c196dda14b1dc8410d22f6d792115
Shashwat Nautiyal <shashwatnautiyal2015@gmail.com>
2022-12-07 11:00:55 +0530

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import create from "zustand";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
+
+interface Theme {
+  mode: "light" | "dark";
+  setTheme: (mode: "light" | "dark") => void;
+}
+
+export const useThemeStore = create<Theme>((set) => ({
+  mode: "light",
+  setTheme: (mode) => {
+    document.documentElement.setAttribute("data-theme", mode);
+    set({ mode: mode });
+  },
+}));
